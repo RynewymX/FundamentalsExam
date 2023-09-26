1e37cfa0e88cfdcdc32894846bf8378f593a71f4
RynewymX <144354513+RynewymX@users.noreply.github.com>
2023-09-26 08:59:17 -0400

    added age temp and toohot

--- python_fundamentals_exam.py
+++ python_fundamentals_exam.py
@@ -3,3 +3,10 @@ print(intro)
 
 first_name = input("What is your first name?: ")
 print(first_name)
+
+age = int(16)
+temperature = float(59.0)
+too_hot = False
+print(type(age))
+print(type(temperature))
+print(type(too_hot))
